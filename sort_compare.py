cd85299b54544dc2810a7a809c51c90718a66f7b
wilsonvetdev <wilsongch92@gmail.com>
2021-02-28 17:26:26 -0500

    sort done

--- sort_compare.py
+++ sort_compare.py
@@ -26,18 +26,18 @@ def shell_sort(a_list):
         for start_position in range(sublist_count):
             gap_insertion_sort(a_list, start_position, sublist_count)
             # print("After increments of size", sublist_count, "The list is", a_list)
-    sublist_count = sublist_count // 2
+        sublist_count = sublist_count // 2
 
 
 def gap_insertion_sort(a_list, start, gap):
     for i in range(start + gap, len(a_list), gap):
         current_value = a_list[i]
         position = i
-    while position >= gap and a_list[position - gap] > current_value:
-        a_list[position] = a_list[position - gap]
-        position = position - gap
+        while position >= gap and a_list[position - gap] > current_value:
+            a_list[position] = a_list[position - gap]
+            position = position - gap
     
-    a_list[position] = current_value
+        a_list[position] = current_value
 
 def python_sort(a_list):
     return sorted(a_list)
@@ -47,11 +47,10 @@ if __name__ == "__main__":
 
     random.seed(100)
 
-    list_sizes = [500, 1000, 3000]
-
-    total_time = 0
+    list_sizes = [500, 1000, 5000]
 
     for list_size in list_sizes:
+        total_time = 0
         for i in range(100):
 
             mylist = get_me_random_list(list_size)
@@ -66,6 +65,7 @@ if __name__ == "__main__":
         print(f"Avg time to sort a list of {list_size} using insertion sort: {avg_time:0.08f} seconds")
     
     for list_size in list_sizes:
+        total_time = 0
         for i in range(100):
             mylist = get_me_random_list(list_size)
             start = time.time()
@@ -77,3 +77,17 @@ if __name__ == "__main__":
         
         avg_time = total_time / 100
         print(f"Avg time to sort a list of {list_size} using python_sort: {avg_time:0.08f} seconds")
+    
+    for list_size in list_sizes:
+        total_time = 0
+        for i in range(100):
+            mylist = get_me_random_list(list_size)
+            start = time.time()
+            # sort function to test 
+            shell_sort(mylist)
+            end = time.time()
+            sort_time = end - start
+            total_time += sort_time
+        
+        avg_time = total_time / 100
+        print(f"Avg time to sort a list of {list_size} using shell_sort: {avg_time:0.08f} seconds")

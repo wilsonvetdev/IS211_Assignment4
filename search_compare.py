73a7fcca9bf91713047289460fd01561d7f842fc
wilsonvetdev <wilsongch92@gmail.com>
2021-02-28 16:18:10 -0500

    wip

--- search_compare.py
+++ search_compare.py
@@ -1,5 +1,6 @@
 
 import random 
+import time
 
 def get_me_random_list(n):
     a_list = list(range(n))
@@ -33,8 +34,56 @@ def ordered_sequential_search(a_list, item):
     
     return found
 
+def binary_search_iterative(a_list, item):
+    first = 0
+    last = len(a_list) - 1
+    found = False 
+
+    while first <= last and not found: 
+        midpoint = (first + last) // 2
+
+        if a_list[midpoint] == item:
+            found = True 
+        else:
+            if item < a_list[midpoint]:
+                last = midpoint - 1 
+            else: 
+                first = midpoint + 1
+    
+    return found 
+
+def binary_search_recursive(a_list, item): 
+    if len(a_list) == 0: 
+        return False
+    else: 
+        midpoint = len(a_list) // 2 
+
+    if a_list[midpoint] == item: 
+        return True 
+    else: 
+        if item < a_list[midpoint]:
+            return binary_search_recursive(a_list[:midpoint], item)
+        else: 
+            return binary_search_recursive(a_list[midpoint + 1:], item)
+
+if __name__ == "__main__":
+
+    list_sizes = [500, 1000, 5000]
+
+    total_time = 0
+
+    for list_size in list_sizes:
+        for i in range(100):
+
+            mylist = get_me_random_list(list_size)
+            start = time.time()
+            # search function to test 
+            end = time.time()
+            sort_time = end - start
+            total_time += sort_time
+        
+        avg_time = total_time / 100
+        print(f"Avg time: {avg_time:0.08f} seconds")
 
 
 
-print(get_me_random_list(10))
-sequential_search()
